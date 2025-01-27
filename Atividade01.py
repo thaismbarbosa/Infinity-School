46796d4d5c75f9e9a25c6d53f023058b1aa022d5
Thaís Maria <104803070+thaismbarbosa@users.noreply.github.com>
2025-01-27 20:53:51 -0300

    Atividade01.py

--- Atividade01.py
+++ Atividade01.py
@@ -1,4 +1,5 @@
 #calcular a media anual de um aluno
+aluno = "Joao"
 media = "4"
 nota_01 = float(input("Digite a 1º nota do bimestre:"))
 nota_02 = float(input("Digite a 2º nota do bimestre:"))
@@ -6,6 +7,6 @@ nota_03 = float(input("Digite a 3º nota do bimestre:"))
 nota_04 = float(input("Digite a 4º nota do bimestre:"))
 
 soma = (nota_01 + nota_02 + nota_03 + nota_04)
-media = (f'A media do aluno é {soma / 4}')
+media = (f'A media anual do {aluno} é {soma / 4}')
 
 print(media)
