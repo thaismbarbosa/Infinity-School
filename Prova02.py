f799ed856e1dd05a85fdc2a8263d55422478ee2b
Thaís Maria <104803070+thaismbarbosa@users.noreply.github.com>
2025-02-04 12:48:54 -0300

    Add files via upload

---
+++ Prova02.py
@@ -0,0 +1,10 @@
+#Crie um programa em Python para verificar se um número é positivo, negativo ou zero. 
+#Utilize estruturas condicionais e formatação com F-string para criar o programa de forma clara.
+
+numero = int(input("Digite um número:"))
+if numero > 0:
+    print(f"O número {numero} é positivo.")
+if numero < 0:
+    print(f" O número {numero} é negativo.")    
+if numero == 0:
+    print(f" O número digitado é zero.")    
