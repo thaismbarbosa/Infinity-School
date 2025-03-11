6a614967938321a76e4e00271c413b96ee27f12e
Thaís Maria <104803070+thaismbarbosa@users.noreply.github.com>
2025-03-11 18:50:33 -0300

    Add files via upload

--- Prova01.py
+++ Prova01.py
@@ -1,11 +1,4 @@
-#Crie uma lista contendo seis frutas de sua escolha. 
-# Depois de ter a lista pronta, converta essa lista em uma tupla. 
-# Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
-
-
-# Criar uma lista de frutas
-minhas_frutas = ["maçã", "pera", "abacate", "kiwi", "banana", "morango"]
-# Converter a lista em uma tupla
-tupla = tuple(minhas_frutas)
-# Exibindo a tupla
-print(tupla)
+base = 10
+altura = 25
+area = (f' A area do reâtangulo é {base * altura}.')
+print(area)
