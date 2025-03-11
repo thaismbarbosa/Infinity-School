5cc9af782a27cf9f6848937efb76534bdd733edd
Thaís Maria <104803070+thaismbarbosa@users.noreply.github.com>
2025-03-11 18:51:19 -0300

    Add files via upload

---
+++ Prova07.py
@@ -0,0 +1,11 @@
+#Crie uma lista contendo seis frutas de sua escolha. 
+# Depois de ter a lista pronta, converta essa lista em uma tupla. 
+# Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+
+# Criar uma lista de frutas
+minhas_frutas = ["maçã", "pera", "abacate", "kiwi", "banana", "morango"]
+# Converter a lista em uma tupla
+tupla = tuple(minhas_frutas)
+# Exibindo a tupla
+print(tupla)
