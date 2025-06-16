3a26fbbcababa35703f5068e13af74765db4d09a
Thaís Maria <104803070+thaismbarbosa@users.noreply.github.com>
2025-06-16 20:18:07 -0300

    Add files via upload

---
+++ Prova14.py
@@ -0,0 +1,36 @@
+class Animal:
+    def __init__(self):
+        pass
+        
+    def falar(self):
+        print('Este animal faz um som genérico.')
+
+
+class Cachorro:
+    def __init__(self, raca, nome):
+        self.raca = raca
+        self.nome = nome
+
+    def falar(self):
+        print(f'O cachorro {self.nome} está latindo!')
+
+meu_cachorro = Cachorro("Pastor-Alemão", "Thor")
+print(meu_cachorro.raca)
+print(meu_cachorro.nome)
+meu_cachorro.falar()
+
+class Gato:
+    def __init__(self, raca, nome):
+        self.raca = raca
+        self.nome = nome
+
+    def falar(self):
+            print(f'O gato {self.nome} está miando!')
+
+meu_gato = Gato("Siamês", "Groot")
+print(meu_gato.raca)
+print(meu_gato.nome)
+meu_gato.falar()
+
+meu_animal = Animal()
+meu_animal.falar()
